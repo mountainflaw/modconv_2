7e4209f2f67007773b66717c93552d6d3f18c756
xor <38231712+mountainflaw@users.noreply.github.com>
2019-05-30 20:02:48 -0400

    add missing comma to primcolor

--- src/material.hxx
+++ src/material.hxx
@@ -75,7 +75,7 @@ class Material
         }
 
         else { /* no texture found when setting up */
-            ret = GetFuckingFrauber() + "gsDPSetPrimColor 0x00, 0x00 128, 128, 128, 0xFF\n";
+            ret = GetFuckingFrauber() + "gsDPSetPrimColor 0x00, 0x00, 128, 128, 128, 0xFF\n";
         }
         return ret;
     }
